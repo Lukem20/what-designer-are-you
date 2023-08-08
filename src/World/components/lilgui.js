11e4bfe6c3282dc581e795fa83ac2f4792549979
Luke Moore <lukemoore237@gmail.com>
2023-08-07 17:41:35 -0700

    Added lil-gui node package for UI debugger.

---
+++ src/World/components/lilgui.js
@@ -0,0 +1,32 @@
+import * as lilgui from 'lil-gui'
+import gsap from 'gsap'
+
+function createGUI (mesh) {
+    const parameters = {
+        color: 0xff0000,
+        spin: () => {
+            gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + 10 })
+        }
+    }
+
+    const gui = new lilgui.GUI();
+    gui
+        .add(mesh.position, 'y')
+        .min(-3)
+        .max(3)
+        .step(0.01)
+        .name('elevation');
+    gui
+        .add(mesh, 'visible');
+    gui
+        .add(mesh.material, 'wireframe');
+    gui
+        .addColor(parameters, 'color')
+        .onChange(() => {
+            mesh.material.color.set(parameters.color);
+        });
+    gui
+        .add(parameters, 'spin');
+}
+
+export { createGUI }
