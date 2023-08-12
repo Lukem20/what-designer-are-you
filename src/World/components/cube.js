0a9dbcff3597657277ccd05761a781e31bd252f3
Luke Moore <lukemoore237@gmail.com>
2023-08-12 11:49:02 -0700

    Fixed assets relative path.

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -27,13 +27,13 @@ function createCube () {
 
         // Create and texture loader and load the texture
         const textureLoader = new TextureLoader(loadingManager);
-        const textureBaseColor = textureLoader.load('src/World/assets/textures/woodendoor/color.jpg');
-        const textureNormalMap = textureLoader.load('src/World/assets/textures/woodendoor/normal.jpg');
-        const textureHeightMap = textureLoader.load('src/World/assets/textures/woodendoor/height.jpg');
-        const textureRoughessMap = textureLoader.load('src/World/assets/textures/woodendoor/roughness.jpg');
-        const textureAmbientOcclusionMap = textureLoader.load('src/World/assets/textures/woodendoor/ambientOcclusion.jpg');
-        const textureMetallic = textureLoader.load('src/World/assets/textures/woodendoor/metalness.jpg');
-        const textureAlpha = textureLoader.load('src/World/assets/textures/woodendoor/alpha.jpg')
+        const textureBaseColor = textureLoader.load('/assets/textures/woodendoor/color.jpg');
+        const textureNormalMap = textureLoader.load('/assets/textures/woodendoor/normal.jpg');
+        const textureHeightMap = textureLoader.load('/assets/textures/woodendoor/height.jpg');
+        const textureRoughessMap = textureLoader.load('/assets/textures/woodendoor/roughness.jpg');
+        const textureAmbientOcclusionMap = textureLoader.load('/assets/textures/woodendoor/ambientOcclusion.jpg');
+        const textureMetallic = textureLoader.load('/assets/textures/woodendoor/metalness.jpg');
+        const textureAlpha = textureLoader.load('/assets/textures/woodendoor/alpha.jpg')
 
         // Create a material using the loaded texture as a color map.
         const material = new MeshStandardMaterial({ 
--- src/World/components/sphere.js
+++ src/World/components/sphere.js
@@ -10,12 +10,12 @@ function createSphere () {
     function createMaterial () {
         // Create and texture loader and load the texture
         const textureLoader = new TextureLoader();
-        const textureBaseColor = textureLoader.load('src/World/assets/textures/planetsurface/planet-surface-albedo.png');
-        const textureNormalMap = textureLoader.load('src/World/assets/textures/planetsurface/planet-surface-normal-dx.png');
-        const textureHeightMap = textureLoader.load('src/World/assets/textures/planetsurface/planet-surface-height.png');
-        const textureRoughessMap = textureLoader.load('src/World/assets/textures/planetsurface/planet-surface-roughness.png');
-        const textureAmbientOcclusionMap = textureLoader.load('src/World/assets/textures/planetsurface/planet-surface-ao.png');
-        const textureMetallic = textureLoader.load('src/World/assets/textures/planetsurface/planet-surface-metallic.png');
+        const textureBaseColor = textureLoader.load('/assets/textures/planetsurface/planet-surface-albedo.png');
+        const textureNormalMap = textureLoader.load('/assets/textures/planetsurface/planet-surface-normal-dx.png');
+        const textureHeightMap = textureLoader.load('/assets/textures/planetsurface/planet-surface-height.png');
+        const textureRoughessMap = textureLoader.load('/assets/textures/planetsurface/planet-surface-roughness.png');
+        const textureAmbientOcclusionMap = textureLoader.load('/assets/textures/planetsurface/planet-surface-ao.png');
+        const textureMetallic = textureLoader.load('/assets/textures/planetsurface/planet-surface-metallic.png');
 
         // Create a material using the loaded texture as a color map.
         const material = new MeshStandardMaterial({ 
